db2eb0b719c8a337fe5419264147b6781cf7e503
Zavier Miller <zavier@resaleai.com>
2020-01-20 12:40:38 -0600

    solved day 2

--- dcp_001.py
+++ dcp_001.py
@@ -22,7 +22,8 @@ def add_to(nums, k):
 print(add_to([10, 10, 3, 7], 20))
 
 # Had to Google, but I understand the logic behind it.
-# Loop through the list of numbers, and check if the target - current num is in set_nums
+# Loop through the list of numbers, and check if the target - num is in set_nums
 # If it is, return True, the two values have been found (if you want to know what it is, return k - num and num)
 # If it isn't, add that number to the set_nums (seperate set, prevents false positives)
 # At the end, if True isn't returned, return False
+# This function runs O(N), one pass :)
--- dcp_002.py
+++ dcp_002.py
@@ -9,3 +9,35 @@
 # [2, 3, 6].
 #
 # Follow-up: what if you can't use division?
+
+from functools import reduce
+
+
+def weirdify_array(arr):
+    new_arr = []
+    total = reduce(lambda x, y: x * y, arr)
+
+    for i in arr:
+        new_arr.append(int(total / i))
+    return new_arr
+
+
+def weirdify_array_no_division(arr):
+    new_arr = []
+
+    for i in arr:
+        value = 1
+        for j in arr:
+            if j != i:
+                value *= j
+        new_arr.append(int(value))
+    return new_arr
+
+
+print(weirdify_array_no_division([1, 2, 3, 4, 5]))
+
+# This one was better, didn't have to Google exact solution.
+# I did however need to Google the reduce function to get the mult total.
+# I think I got the best time for no div possible but I dont know. It is O(N^2)
+# as it needs to loop through the array N times for each pass.
+# The division allowed one is O(2N) which makes it as efficient as possible (linear)
