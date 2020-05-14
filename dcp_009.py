73b2f5ef9b9fe51aaecec1f5e474ac0fc8d4b74f
Zavier Miller <xavier2miller@gmail.com>
2020-05-13 21:10:27 -0500

    completed day 9

--- dcp_009.py
+++ dcp_009.py
@@ -7,3 +7,24 @@
 # 1, 5] should return 10, since we pick 5 and 5.
 # 
 # Follow-up: Can you do this in O(N) time and constant space?
+
+def largest_na_sum(nums):
+    queue = []
+    queue.append((0,0))
+
+    totals = []
+
+    while queue:      
+        s = queue.pop(0)
+        i, prev = s
+        prev += nums[i]
+        if i + 2 < len(nums):
+            for j in range(i + 2, len(nums)):
+                queue.append((j,prev))
+        else:
+            totals.append(prev)
+    
+    return max(totals)
+
+
+print(largest_na_sum([5,1,1,5]))
